03c3c8646b162c9505d5113deb055e93b0c2e44c
ukrwebprom <ukrwebprom@gmail.com>
2022-12-22 13:07:40 +0200

    initial update

---
+++ src/js/01-gallery.js
@@ -0,0 +1,25 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const gallery = makeGallery(galleryItems);
+galleryContainer.insertAdjacentHTML("afterbegin", gallery);
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionSelector: "img",
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function makeGallery(galleryData) {
+  return galleryData
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`;
+    })
+    .join("");
+}
+
+console.log(galleryItems);
